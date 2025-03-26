a1d46eb56ded9dc17b9962ef45f1353ae555d04d
vaibhav-karnwal <vaibhavkarnwal2812@gmail.com>
2025-03-27 00:25:57 +0530

    changes pushed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/yumyum/", // Set this to match your GitHub repository name
+});
